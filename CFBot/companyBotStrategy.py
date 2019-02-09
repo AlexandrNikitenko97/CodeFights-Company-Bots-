46ea9a4dc3e69eaa153a801606b43ed17a6f1f93
Alexandr Nikitenko <sashanikitenko74@gmail.com>
2019-02-09 18:40:56 +0200

    Update companyBotStrategy.py

--- CFBot/companyBotStrategy.py
+++ CFBot/companyBotStrategy.py
@@ -1,3 +1,14 @@
 def companyBotStrategy(trainingData):
     answers = [trainingData[i][0] for i in range(len(trainingData)) if trainingData[i][1] == 1]
     return (sum(answers))/len(answers) if len(answers) > 0 else 0
+
+
+# more readable variant
+def companyBotStrategy(trainingData):
+    divider = correct = 0
+    for i in trainingData:
+        if i[1] == 1:
+            correct += i[0]
+            divider += 1
+    return 0.0 if dividor == 0 else correct/divider
+            
