5088512878df5885bc8fbab43f9c16a9c746ccb9
Alexandr Nikitenko <sashanikitenko74@gmail.com>
2019-02-10 20:07:24 +0200

    Update launchSequenceChecker.py

--- SpaceBot/launchSequenceChecker.py
+++ SpaceBot/launchSequenceChecker.py
@@ -18,3 +18,15 @@ def launchSequenceChecker(systemNames, stepNumbers):
                 result.append(True)
     return True if sum(result) == len(matched) else False
         
+
+    
+# another way
+def launchSequenceChecker(systemNames, stepNumbers):
+    system_dict = {name:[] for name in systemNames}
+    for i in range(len(systemNames)):
+        system_dict[systemNames[i]].append(stepNumbers[i])
+    for step in system_dict.values():
+        for each in range(len(step)-1):
+            if step[each] >= step[each+1]:
+                return False
+    return True
